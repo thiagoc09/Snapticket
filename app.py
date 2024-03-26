3cfdc0daf1a417d8b4306bbc7d56e3fe2ee9d19f
Thiago Araujo <thiagoc09@live.com>
2024-03-26 00:34:58 -0300

    banco de dados e flask migrate

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
-# app.py
-from app import create_app
+# Dentro do seu arquivo onde a função create_app é definida, como app/__init__.py
+from config import Config
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
-app = create_app()
+db = SQLAlchemy()
+migrate = None
 
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+
+    global migrate
+    migrate = Migrate(app, db)
+
+    # Importar os modelos aqui para que eles sejam conhecidos pelo Flask-Migrate
+    from app import models
+
+    # Aqui você pode registrar seus blueprints e outras configurações
+
+    return app
